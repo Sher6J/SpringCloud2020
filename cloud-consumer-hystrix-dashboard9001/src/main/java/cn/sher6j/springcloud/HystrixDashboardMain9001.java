2d17818a3afd98413058179ce01764e0568818c8
Sher6J <liujx17614417627@outlook.com>
2020-05-22 00:06:41 +0800

    openfeign

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/cn/sher6j/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package cn.sher6j.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author sher6j
+ * @create 2020-05-21-23:47
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+}
