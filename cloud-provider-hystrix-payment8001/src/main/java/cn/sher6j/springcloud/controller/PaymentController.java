19564b76c452d90f0bb67470e432fc8c84ba0b18
Sher6J <liujx17614417627@outlook.com>
2020-05-21 23:05:58 +0800

    openfeign

--- cloud-provider-hystrix-payment8001/src/main/java/cn/sher6j/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/sher6j/springcloud/controller/PaymentController.java
@@ -35,4 +35,13 @@ public class PaymentController {
         log.info("========result:" + result);
         return result;
     }
+
+
+    //=====服务熔断
+    @GetMapping("payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Long id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("=========result:" + result);
+        return result;
+    }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/cn/sher6j/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/sher6j/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package cn.sher6j.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -49,5 +51,41 @@ public class PaymentService {
      */
     public String paymentInfo_TimeOutHandler(Long id) {
         return "系统忙，请稍后再试";
+    }
+
+    //======服务熔断
+
+    /**
+     * fallbackMethod                               服务降级方法
+     * circuitBreaker.enabled                       是否开启断路器   默认为true
+     * circuitBreaker.requestVolumeThreshold        请求次数    默认20
+     * circuitBreaker.sleepWindowInMilliseconds     时间窗口期  默认5000
+     * circuitBreaker.errorThresholdPercentage      失败率达到多少后跳闸 默认50
+     * 以下配置意思是在10秒时间内请求10次，如果有6此是失败的，就触发熔断器
+     * 注解@HystrixProperty中的属性在com.netflix.hystrix.HystrixCommandProperties类中查看
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Long id) {
+        if (id < 0) {
+            throw new RuntimeException("id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + " 调用成功，流水号： " + serialNumber;
+    }
+
+    /**
+     * 服务熔断触发的服务降级方法
+     * @param id
+     * @return
+     */
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Long id) {
+        return "id 不能为负数，请稍后再试。id:" + id;
     }
 }
