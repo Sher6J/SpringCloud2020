f5a4c1f812003bd87fa5a0c87b022f9a44a949da
Sher6J <liujx17614417627@outlook.com>
2020-05-28 22:40:48 +0800

    Nacos作为配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/sher6j/springcloudalibaba/NacosConfigMain3377.java
@@ -0,0 +1,17 @@
+package cn.sher6j.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author sher6j
+ * @create 2020-05-28-21:06
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigMain3377.class, args);
+    }
+}
